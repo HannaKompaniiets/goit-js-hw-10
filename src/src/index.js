2fad674b0dd83a7693aba380b7e0db9440a03779
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-04-14 12:50:20 +0200

    Update index.js

--- src/src/index.js
+++ src/src/index.js
@@ -2,28 +2,44 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const url = 'https://restcountries.com';
+const url = 'https://restcountries.com/v3.1';
 const options = {
-    name.official,
-capital,
-population ,
-flags.svg,
-languages
+    official,
+    capital,
+    population ,
+    flags,
+    languages
 }
 
 
 const input = document.querySelector('#search-box');
-
+const countryList = document.querySelector('.country-list');
 input.addEventListener('input', _.delay(getSearch, DEBOUNCE_DELAY));
 
-function getSearch() {
-    console.log(input.current.target.value);
+function getSearch(name) {
+    const name = (input.current.target.value);
 }
 
 
 
 fetchCountries(name){ 
-fetch()
+    fetch(url, options)
+        .then(response => {
+            return https://restcountries.com/v3.1/name/{name}
+        })
+        .then(data => {
+            const markup = `
+            <li> https://restcountries.com/v3.1/name/{'official'}</li>
+            <li> https://restcountries.com/v3.1/name/{'capital'}</li>
+            <li> https://restcountries.com/v3.1/name/{'population'}</li>
+            <li> https://restcountries.com/v3.1/name/{'flags'}</li>
+            <li> https://restcountries.com/v3.1/name/{'languages'}</li>
+             `
+            countryList.insertAdjacentHTML('beforeend', markup);
+  })
+  .catch(error => {
+    // Error handling
+  });
 
 }
 
