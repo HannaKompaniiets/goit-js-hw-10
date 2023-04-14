093746ef0b6e2b547b2c9c8796764d695782fb88
Hanna Kompaniiets <anna.komp24@gmail.com>
2023-04-14 12:28:35 +0200

    1

--- src/src/index.js
+++ src/src/index.js
@@ -1,3 +1,36 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const url = 'https://restcountries.com';
+const options = {
+    name.official,
+capital,
+population ,
+flags.svg,
+languages
+}
+
+
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', _.delay(getSearch, DEBOUNCE_DELAY));
+
+function getSearch() {
+    console.log(input.current.target.value);
+}
+
+
+
+fetchCountries(name){ 
+fetch()
+
+}
+
+
+
+
+// _.delay(function(text) {
+//   console.log(text);
+// }, 1000, 'later');
+// // => Logs 'later' after one second.
